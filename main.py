0f065e977eef36e19fc0c746080a7848e4bd173e
KaRaBe11a <reznikov_den@inbox.ru>
2026-04-22 14:42:26 +0300

    fix

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ def main():
     feature_extractor = FeatureExtractor()
     classifier = VesselClassifier()
 
-    test_text = "Круизное морское судно для перевозки пассажиров"
-    
-    # Тестирование иерархического подхода (по умолчанию)
-    print("\n" + "="*60)
-    print("ТЕСТ 1: ИЕРАРХИЧЕСКИЙ ПОДХОД (use_hierarchical=True)")
-    print("="*60)
-    result_hierarchical = classifier.classify(test_text, use_hierarchical=True)
-    classifier.show_result(result_hierarchical)
+    test_text = [
+        "Круизное судно для перевозки пассажиров по морю",
+        "Морской танкер для транспортировки сырой нефти, дедвейт 150000 тонн",
+        "Атомный ледокол для прокладки маршрутов во льдах Арктики",
+        "Контейнеровоз для перевозки стандартизированных контейнеров по мировому океану",
+        "Пассажирско-автомобильный паром для прибрежных перевозок",
+        "Научно-исследовательское судно для океанографических измерений без груза"
+    ]
+    for i in test_text:
+        # Тестирование иерархического подхода (по умолчанию)
+        print("\n" + "="*60)
+        print("ТЕСТ 1: ИЕРАРХИЧЕСКИЙ ПОДХОД (use_hierarchical=True)")
+        print("="*60)
+        result_hierarchical = classifier.classify(i, use_hierarchical=True)
+        classifier.show_result(result_hierarchical)
 
-    # Тестирование классического подхода
-    print("\n" + "="*60)
-    print("ТЕСТ 2: КЛАССИЧЕСКИЙ ПОДХОД (use_hierarchical=False)")
-    print("="*60)
-    result_classic = classifier.classify(test_text, use_hierarchical=False)
-    classifier.show_result(result_classic)
+        # Тестирование классического подхода
+        print("\n" + "="*60)
+        print("ТЕСТ 2: КЛАССИЧЕСКИЙ ПОДХОД (use_hierarchical=False)")
+        print("="*60)
+        result_classic = classifier.classify(i, use_hierarchical=False)
+        classifier.show_result(result_classic)
 
 
 if __name__ == '__main__':
